cd878784b4589157974dfe4471ca0ecdaa093128
rvbcia <dagryba@gmail.com>
2018-03-29 18:30:29 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+// W oparciu o materiały podane na wykładzie proszę dokonać z wykorzystaniem notatnika implementacji pierwszej aplikacji wyświetlającej na ekranie napis „Hello World”.
+
+print "Hello World"
